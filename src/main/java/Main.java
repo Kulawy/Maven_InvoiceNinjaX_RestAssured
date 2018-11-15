8771aa882dfee5fc99557c6e7dad1ee52debeae5
jgeron <jgeron@pl.sii.eu>
2018-11-15 13:03:04 +0100

    After Code Cleaning.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args){
 
-        System.out.println("Siemaneczko");
+        System.out.println("Testy RestAssured strony NinjaX");
     }
 
 }
--- src/test/java/RestTest_NinjaX/ClientsTest.java
+++ src/test/java/RestTest_NinjaX/ClientsTest.java
@@ -21,17 +21,30 @@ import static org.hamcrest.Matchers.equalTo;
 
 public class ClientsTest {
 
-    RestAssured xRA;
-    RequestSpecification httpRequest;
-    Response response;
+    private static final String HEADER_OPTION_01 = "Content-Type";
+    private static final String HEADER_OPTION_02 = "application/json";
+    private static final String HEADER_VAL = "X-Ninja-Token";
+    private static final String TOKEN = "pwamm76crdewrh9ldxssghfcebjdw2pl";
+    private static final String ID = "/80";
+    private static final int STATUS_OK = 200;
+
+
+    private static final String numberOfClientToDelete = "93";
+
+    private StringBuilder sB;
+    private RestAssured xRA;
+    private RequestSpecification httpRequest;
+    private Response response;
+
 
 
     @BeforeMethod
     public void setUp() {
         xRA = new RestAssured();
+        sB = new StringBuilder();
         xRA.baseURI = "http://79.137.68.21/api/v1/clients";
         httpRequest = xRA.given();
-        httpRequest.header("Content-Type", "application/json").and().header("X-Ninja-Token", "pwamm76crdewrh9ldxssghfcebjdw2pl");
+        httpRequest.header(HEADER_OPTION_01, HEADER_OPTION_02).and().header(HEADER_VAL, TOKEN);
     }
 
     @AfterMethod
@@ -42,76 +55,69 @@ public class ClientsTest {
     }
 
     @Test
-    public void testClient80id() {
-        /*response = httpRequest.get("/80");
-        String bodyAsString = response.getBody().asString();
-        Assert.assertTrue(bodyAsString.contains("\"id\": 80"));
-        System.out.println(bodyAsString);*/
-
-        String bodyAsString = httpRequest.get("/80").getBody().asString();
-        Assert.assertTrue(bodyAsString.contains("\"id\": 80"));
+    public void testClientById() {
+        String bodyAsString = httpRequest.get(ID).getBody().asString();
+        Assert.assertTrue(bodyAsString.contains(sB.append("\"id\": ").append(ID).toString()));
         System.out.println(bodyAsString);
 
-        /* //Albo jedna linijka
-        Assert.assertTrue(
-                httpRequest.get("/80").getBody().asString().contains("\"id\": 80")
-        );*/
     }
 
-    //BEST WAY TO WRITE TEST in my opinion
     @Test
     public void testGetAllClients(){
         response = httpRequest
                 .get()
                 .then()
                 .assertThat()
-                .statusCode(200)
+                .statusCode(STATUS_OK)
                 .extract()
                 .response();
 
         System.out.println(response.getBody().asString());
-
-        Gson gson = new Gson();
-        JsonObject jsonObject = gson.fromJson(response.getBody().asString(), JsonObject.class);
-        //JsonObject data = jsonObject.get("data").getAsJsonObject();
-        JsonArray data = jsonObject.get("data").getAsJsonArray();
-        List<JsonElement> jsonList = new LinkedList<>();
-        for( int i = 0; i < data.size() ; i++){
-            jsonList.add(data.get(i));
-        }
-
-        System.out.println("count of clients: " +  data.size());
-        //System.out.println(data.toString());
+        JsonArray jA = getJsonArrayFromBody();
+        System.out.println(sB.append("count of clients: ").append(jA.size()).toString());
+        System.out.println(jA.get(0));
 
     }
 
+    private JsonArray getJsonArrayFromBody(){
+        Gson gson = new Gson();
+        JsonObject jsonObject = gson.fromJson(response.getBody().asString(), JsonObject.class);
+        JsonArray dataJsonArray = jsonObject.get("data").getAsJsonArray();
+        return dataJsonArray;
+    }
+
     @Test
-    public void testClient80idWithOutPreSet() {
+    public void testClientIdWithOutPreSet() {
+        String addressToSend = "http://79.137.68.21/api/v1/clients/";
+
         given()
-                .header("Content-Type", "application/json")
+                .header(HEADER_OPTION_01, HEADER_OPTION_02)
                 .and()
-                .header("X-Ninja-Token", "pwamm76crdewrh9ldxssghfcebjdw2pl")
+                .header(HEADER_VAL, TOKEN)
         .expect()
-                .body("data.id", equalTo(80))
-                .when().get("http://79.137.68.21/api/v1/clients/80")
+                .body("data.id", equalTo(ID))
+                .when().get(sB.append(addressToSend).append(ID).toString())
         .print();
     }
 
     @Test
     public void testAllClientsLoading() {
+        String addressToSend = "http://79.137.68.21/api/v1/clients";
         given()
-                .header("Content-Type", "application/json")
+                .header(HEADER_OPTION_01, HEADER_OPTION_02)
                 .and()
-                .header("X-Ninja-Token", "pwamm76crdewrh9ldxssghfcebjdw2pl").
+                .header(HEADER_VAL, TOKEN).
         when()
-                .get("http://79.137.68.21/api/v1/clients").
-        then().assertThat().statusCode(200).extract().response().print();
+                .get(addressToSend).
+        then().assertThat().statusCode(STATUS_OK).extract().response().print();
     }
 
     @Test
     public void testClientPost(){
-        String nameOfClient = "JavaRestAsuredTest00";
+        String nameOfClient = "JavaRestAssuredTest00";
         String cityOfClient = "Krakow";
+        String propName = "data.name";
+        String propCity = "data.city";
 
         JsonObject jsonClient = new JsonObject();
         jsonClient.addProperty("name", nameOfClient);
@@ -122,24 +128,22 @@ public class ClientsTest {
                 .post()
                 .then()
                 .assertThat()
-                .statusCode(200)
-                .body("data.name", equalTo(nameOfClient))
-                .body("data.city", equalTo(cityOfClient))
+                .statusCode(STATUS_OK)
+                .body(propName, equalTo(nameOfClient))
+                .body(propCity, equalTo(cityOfClient))
                 .extract()
                 .response();
-
         response.print();
 
     }
 
     @Test
     public void testClientDelete(){
-        String numberOfClientToDelete = "93";
         response = httpRequest
                 .delete("/" + numberOfClientToDelete)
                 .then()
                 .assertThat()
-                .statusCode(200)
+                .statusCode(STATUS_OK)
                 .extract()
                 .response();
         response.print();
@@ -157,17 +161,17 @@ public class ClientsTest {
 
     @Test(dataProvider="clientsToGet")
     public void testGetClientsById(int clientId) {
-        String clientUri = "/" + String.valueOf(clientId);
+        String valToCompare = "data.id";
+
         response = httpRequest
-                .get(clientUri)
+                .get(sB.append("/").append(clientId).toString())
                 .then()
                 .assertThat()
-                .statusCode(200)
+                .statusCode(STATUS_OK)
                 .assertThat()
-                .body("data.id", equalTo(clientId))
+                .body( valToCompare, equalTo(clientId))
                 .extract()
                 .response();
-
         response.print();
 
     }
